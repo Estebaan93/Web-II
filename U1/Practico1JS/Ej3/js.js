9aab3727fda4b72a1c07023f9248d57bcb66b45e
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2024-09-02 08:05:18 +0000

    Salida por consola

--- U1/Practico1JS/Ej3/js.js
+++ U1/Practico1JS/Ej3/js.js
@@ -7,13 +7,13 @@ const round = (numero, digitos) => {
 const resultado1 = round(1.005, 2);
 const resultado2 = round(1.05, 2);
 const resultado3 = round(1.0005, 2);
-
+/*
 document.getElementById("n1").innerHTML = `(1.005, 2) Redondeo: ${resultado1}`;
 document.getElementById("n2").innerHTML = `(1.05, 2) Redondeo: ${resultado2}`;
-document.getElementById("n3").innerHTML = `(1.0005, 2) Redondeo: ${resultado3}`;
+document.getElementById("n3").innerHTML = `(1.0005, 2) Redondeo: ${resultado3}`;*/
 
-/*console.log(`round(1.005, 2) Redondeo: ${round(1.005, 2)}`);	// 1.01
+console.log(`round(1.005, 2) Redondeo: ${round(1.005, 2)}`);	// 1.01
 console.log(`round(1.05, 2) Redondeo: ${round(1.05, 2)}`);	//1.05
-console.log(`round(1.0005, 2) Redondeo: ${round(1.0005, 2)}`);	//1 */
+console.log(`round(1.0005, 2) Redondeo: ${round(1.0005, 2)}`);	//1 
 
 
--- U1/Practico1JS/Ej4/js.js
+++ U1/Practico1JS/Ej4/js.js
@@ -3,7 +3,7 @@ const reverseString = str => [...str].reverse().join('');
 const frase= "Hola mundo cruel";
 const fraseReves= reverseString(frase);
 
-/*console.log("Frase original: "+frase);
-console.log("Frase al reves: "+fraseReves);*/
+console.log("Frase original: "+frase);
+console.log("Frase al reves: "+fraseReves);
 
-document.getElementById('parr').innerHTML=`Frase original: ${frase} <br> Frase al reves: ${fraseReves}`;
+/*document.getElementById('parr').innerHTML=`Frase original: ${frase} <br> Frase al reves: ${fraseReves}`;*/
