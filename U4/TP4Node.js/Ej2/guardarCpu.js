21ca9d1d35dd7f2201db92262d4bdbf3d2d3166d
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2025-03-02 16:32:01 +0000

    Probando

--- U4/TP4Node.js/Ej2/guardarCpu.js
+++ U4/TP4Node.js/Ej2/guardarCpu.js
@@ -9,8 +9,28 @@ const fs=require('fs');
 //Obtener info de la cpu
 const cpuInfo=os.cpus();
 
+//Obtener datos de ram
+const totalMemoria= os.totalmem();
+const memoriaLibre= os.freemem();
+
+//Convertir a megabytes
+const memoriaTotalMB=(totalMemoria/1024/1024).toFixed(2);
+const memoriaLibreMB= (memoriaLibre/ 1024/ 1024).toFixed(2);
+
+const systemInfo = {
+  cpus: cpuInfo.map(cpu => ({
+    model: cpu.model,
+    speed: cpu.speed,
+    times: cpu.times,
+  })),
+  ram: {
+    total: `${memoriaTotalMB} MB`,
+    free: `${memoriaLibreMB} MB`,
+    used: `${(memoriaTotalMB - memoriaLibreMB).toFixed(2)} MB`,
+  }
+};
 //conventir a JSON
-const cpuInfoJson=JSON.stringify(cpuInfo,null,2);
+const cpuInfoJson=JSON.stringify(systemInfo,null,2);
 
 //Ruta del archivo donde se guarda la informacion
 const filePath='cpu_info.txt';
